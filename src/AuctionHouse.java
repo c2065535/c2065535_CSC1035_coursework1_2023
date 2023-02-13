12f7f8f7abe10d9877c9005011888fec3a5bfe9e
c2065535 <D.Leivdal2@newcastle.ac.uk>
2023-02-13 11:14:00 +0000

    Add Getter and Setter methods for AuctionHouse

--- src/AuctionHouse.java
+++ src/AuctionHouse.java
@@ -1,4 +1,28 @@
 public class AuctionHouse {
     private String auctionHouseName;
     private String itemSold[];
+
+    public String getAuctionHouseName(){
+        return auctionHouseName;
+    }
+    public void setAuctionHouseName(String auctionHouseName){
+        this.auctionHouseName = auctionHouseName;
+    }
+
+    public String[] getitemSold(){
+        return itemSold;
+    }
+
+    public void setitemSold(String[] itemSold){
+        this.itemSold = itemSold;
+    }
+
+    public AuctionHouse(String auctionHouseName, String[] itemSold){
+        this.auctionHouseName = auctionHouseName;
+        this.itemSold = itemSold;
+    }
+
+    public String toString() {
+        return String.format("%s%s", getAuctionHouseName(), getitemSold());
+    }
 }
