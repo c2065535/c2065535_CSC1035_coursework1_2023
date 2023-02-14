346777820078a28b6decd979a4f414aa5b2fdf41
c2065535 <D.Leivdal2@newcastle.ac.uk>
2023-02-14 13:14:09 +0000

    Add findHighestPrice Add findAveragePrice Add soldOverPrice

--- src/AuctionHouse.java
+++ src/AuctionHouse.java
@@ -1,14 +1,23 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class AuctionHouse {
 
-    // How to get itemSold to link with an Item from the Item class?
-    // e.g. AuctionHouse(car) = {item_lot_number, buyerName, itemPrice, yearItemSold, itemType}
-    // AuctionHouse(Painting) = {item_lot_number, buyerName, itemPrice, yearItemSold, itemType}
-
     private Item i;
     private String auctionHouseName;
-    private String itemSold[];
+    //private String itemSold[];
+
+    private List<Item> soldItems;
+
+    //private Map<Boolean, Item> Items;
+
+
+    public AuctionHouse(String auctionHouseName, List<Item> soldItems){
+        this.auctionHouseName = auctionHouseName;
+        this.soldItems = soldItems;
+    }
+
 
     public String getAuctionHouseName(){
         return auctionHouseName;
@@ -17,21 +26,69 @@ public class AuctionHouse {
         this.auctionHouseName = auctionHouseName;
     }
 
-    public String[] getitemSold(){
-        return itemSold;
+
+    public List<Item> getItemSold(){
+        return soldItems;
     }
 
+
+    public boolean addToAuctionHouse(Item item){
+
+        if (soldItems.contains(item)){
+            // dont add to list or map
+        }else{
+            soldItems.add(item);
+        }
+        return true;
+    }
+
+    // https://stackoverflow.com/questions/19338686/getting-max-value-from-an-arraylist-of-objects
+    // Stream through items sold, compare itemPrice, return item with max price.
+    public Item findHighestPrice(){
+        return getItemSold().stream().max(Comparator.comparing(Item::getItemPrice)).get();
+    }
+
+    // https://stackoverflow.com/questions/10791568/calculating-average-of-an-array-list#:~:text=From%20Java8%20onward%20you%20can%20get%20the%20average,has%20the%20advantage%20of%20having%20no%20moving%20parts.
+    // Method for finding Average price of a Auction House
+    public double findAveragePrice(){
+        // Stream through items sold, add up item price, return average price. Else - return 0.
+        return getItemSold().stream().mapToInt(Item::getItemPrice).average().orElse(0.0);
+    }
+
+    // Method for finding Items sold over specific price.
+    public List<Item> soldOverPrice(int price){
+        // Make result a list to be returned
+        List<Item> result = new ArrayList<>();
+
+        // For every item sold
+        for (Item item : getItemSold()){
+            // If item sold is over price
+            if (item.getItemPrice() > price){
+                // add Item to result list
+                result.add(item);
+            }
+        }
+        return result;
+    }
+
+    /*
     public void setitemSold(String[] itemSold){
         this.itemSold = itemSold;
     }
+    */
 
-    public AuctionHouse(String auctionHouseName, String[] itemSold){
-        this.auctionHouseName = auctionHouseName;
-        this.itemSold = itemSold;
-    }
+    //public String toString() {
+      //  return String.format("Auction House: %s\nItems Sold: %s", getAuctionHouseName(), Arrays.toString(getitemSold()));
+    //}
 
+    @Override
     public String toString() {
-        return String.format("Auction House: %s\nItems Sold: %s", getAuctionHouseName(), Arrays.toString(getitemSold()));
+        return "AuctionHouse{" +
+                "i=" + i +
+                ", auctionHouseName='" + auctionHouseName + '\'' +
+                //", itemSold=" + Arrays.toString(itemSold) +
+                ", soldItems=" + soldItems +
+                //", Items=" + Items +
+                '}';
     }
-
 }
--- src/main.java
+++ src/main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class main {
     private Item i;
     private AuctionHouse a;
@@ -6,6 +9,7 @@ public class main {
         this.i = i;
         this.a = a;
     }
+    /*
     public Item getI(){
         return i;
     }
@@ -18,11 +22,45 @@ public class main {
     public void setA(AuctionHouse a){
         this.a = a;
     }
+     */
 
     public static void main(String[] args) {
-        Item i = new Item(10, "Charlie", 100, 2013, "Sculpture");
-        System.out.println(i);
-        AuctionHouse a = new AuctionHouse("Randy", new String[]{"cat", "rat", "pig", "dog"});
-        System.out.println(a);
+
+        List<Item> a = new ArrayList<>();
+
+        // TEST
+        // Auction house G
+        AuctionHouse g = new AuctionHouse("Randy", a);
+        // Add Item i1 & i2 to Auction house G
+        Item i1 = new Item(10, "Charlie", 100, 2013, "Sculpture");
+        a.add(i1);
+        Item i2 = new Item(101, "Charles", 10, 2013, "Sculpture");
+        g.addToAuctionHouse(i2);
+
+        /*
+        System.out.println("----");
+        System.out.println(g.soldOverPrice(11));
+        System.out.println("----");
+
+        a.add(i2);
+         */
+
+        // TEST
+        // Auction house h
+        AuctionHouse h = new AuctionHouse("Randy1", new ArrayList<Item>());
+        // Add Item j1 & j2 to Auction house G
+        Item j1 = new Item(10, "Charlie1", 100, 2014, "Sculpture");
+        h.addToAuctionHouse(j1);
+        Item j2 = new Item(103, "Charlie2", 111, 2016, "Sculpture");
+        h.addToAuctionHouse(j2);
+
+        System.out.println(g);
+        System.out.println("----");
+        System.out.println(h);
+
+
+        //System.out.println(i);
+        //AuctionHouse a = new AuctionHouse("Randy", new String[]{"cat", "rat", "pig", "dog"});
+        //System.out.println(a);
     }
 }
